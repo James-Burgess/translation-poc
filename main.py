88b1b7dfa3288193a2938f938f85570a6f437c26
james-prodigy <james.burgess@prodigyfinance.com>
2020-07-08 10:17:12 +0200

    Initail commit of POC image OCR and Translation
    Signed-off-by: james-prodigy <james.burgess@prodigyfinance.com>

---
+++ main.py
@@ -0,0 +1,49 @@
+from flask import *
+import os
+from ocr import do_ocr
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# searchword = request.args.get('q', '')
+
+UPLOAD_FOLDER = './'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = file.filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            print(file_path)
+            doc_text = do_ocr(file_path)
+
+            return doc_text
+        else:
+            return "could not translate file"
+
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
